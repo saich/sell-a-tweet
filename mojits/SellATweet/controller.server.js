bf8ffb575a75c096afd9eb09bd99ba94607b6c0d
Sai Prasad <sai@gwynniebee.com>
2013-09-07 13:43:00 +0530

    /logout URL added to cleanup session & revoke instamojo's token.

--- mojits/SellATweet/controller.server.js
+++ mojits/SellATweet/controller.server.js
@@ -12,7 +12,19 @@ YUI.add('SellATweet', function(Y, NAME) {
              ac.done({
                 status: ac.session.get("status")
             });
+        },
+
+        logout: function (ac) {
+            // destroy instamojo token
+            var im = new Y.InstaMojo({
+                authtoken: ac.session.get("imtoken")
+            });
+            im.deleteAuthToken(function () {
+                // ignore error, since there could be no token at all..
+                ac.session.destroy();
+                ac.http.redirect("/", 303);
+            });
         }
     };
 
-}, '0.0.1', {requires: ['mojito', 'mojito-session-addon']});
+}, '0.0.1', {requires: ['mojito', 'mojito-session-addon', 'mojito-http-addon', 'instamojo']});
